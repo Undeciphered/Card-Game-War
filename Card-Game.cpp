7ac611095c763cc5a5ba8a8cc23a0e57d51fa938
Undeciphered <arseneault3luc@gmail.com>
2025-04-09 11:35:53 -0300

    Added the rest of the game
    idk man i guess it was just a realy easy project maybe il make other card games now idk

--- Card-Game.cpp
+++ Card-Game.cpp
@@ -7,8 +7,11 @@ struct card {
     std::string suit{""};
     std::string rank{""};
     int value{0};
-    card(std::string &suit, std::string &rank, int &value)
+    card(std::string suit, std::string rank, int value)
         : suit(suit), rank(rank), value(value) {}
+    std::string card_info() {
+        return (rank + " of " + suit);
+    }   
 };
 
 class deck {
@@ -50,14 +53,120 @@ class deck {
         }
 };
 
+class player {
+    private:
+        std::string name{""};
+        std::vector<card> hand; // this is used as the players deck in war
+    
+    public:
+        player(std::string player_name) : name(player_name) {}
+        
+        void draw_card(deck &deck_to_draw_from) {
+                hand.push_back(deck_to_draw_from.draw_card());
+            }
+            
+        card draw_first_card () { // draws the first card in hand, or from the players deck in war
+            card first_card_in_hand = hand.front();
+            hand.erase(hand.begin());
+            return first_card_in_hand;
+        }  
+        
+        void pick_up_card(card &card_picked_up) {
+            hand.push_back(card_picked_up);
+        }
+        
+        int card_count() {
+            return hand.size();   
+        }
+        
+        std::string get_name() {
+            return name;   
+        }
+};
+
+class war {
+    private:
+        card player_one_card;
+        card player_two_card;
+        player player_one;
+        player player_two;
+        std::vector<card> war_pile;
+        bool autorun{false};
+    
+    public:
+        war(std::string player_one_name, std::string player_two_name, bool autorun) : player_one_card("none", "none", 0), player_two_card("none", "none", 0), player_one(player_one_name), player_two(player_two_name), autorun(autorun) {}
+    
+        void setup_game() {
+            deck mydeck;
+            mydeck.make_deck();
+            mydeck.shuffle_deck();
+            for(int i = 0; i < 52; i++) { // splits the deck in two for both players decks 
+                if((i & 1) == 0) {
+                    player_one.draw_card(mydeck);
+                } else {
+                    player_two.draw_card(mydeck);
+                }
+            } 
+        }
+        
+        void battle() {
+            player_one_card = player_one.draw_first_card();
+            player_two_card = player_two.draw_first_card();
+            std::cout << player_one.get_name() << " drew the " << player_one_card.rank << " of " << player_one_card.suit << '\n'; 
+            std::cout << player_two.get_name() << " drew the " << player_two_card.rank << " of " << player_two_card.suit << '\n';
+            if(player_one_card.value == player_two_card.value) {
+                war_pile.push_back(player_one_card);
+                war_pile.push_back(player_two_card);
+                std::cout << "its a tie go to war\n\n";
+                battle();
+            } else if(player_one_card.value > player_two_card.value) {
+                std::cout << player_one.get_name() << " wins and takes the " << player_two_card.card_info() << " and the " << player_one_card.card_info() << '\n';
+                player_one.pick_up_card(player_two_card);
+                player_one.pick_up_card(player_one_card);
+                if(war_pile.size() != 0) {
+                    std::cout << player_one.get_name() << " won the war and took the cards: ";
+                    for(card selected_card : war_pile) {
+                        std::cout << selected_card.card_info() << ", ";
+                        player_one.pick_up_card(selected_card);
+                    }
+                    war_pile.clear();
+                    std::cout << '\n';
+                }
+            } else {
+                std::cout << player_two.get_name() << " wins and takes the " << player_one_card.card_info() << " and the " << player_two_card.card_info() << '\n';
+                player_two.pick_up_card(player_one_card);
+                player_two.pick_up_card(player_two_card);
+                if(war_pile.size() != 0) {
+                    std::cout << player_two.get_name() << " won the war and took the cards: ";
+                    for(card selected_card : war_pile) {
+                        std::cout << selected_card.card_info() << ", ";
+                        player_two.pick_up_card(selected_card);
+                    }
+                    war_pile.clear();
+                    std::cout << '\n';
+                }
+            } 
+        }
+        
+        void start_game() {
+            while(player_one.card_count() != 0 && player_two.card_count() != 0) {
+                battle();
+                if(autorun) {std::cout << '\n';}
+                else{std::cin.get();}
+            }
+            if(player_one.card_count() < player_two.card_count()) {
+                std::cout << player_two.get_name() << " won because " << player_one.get_name() << " ran out of cards\n";
+            } else {
+                std::cout << player_one.get_name() << " won because " << player_two.get_name() << " ran out of cards\n";
+            }   
+        }
+};
+
 int main() {
     
-    
-    deck mydeck;
-    mydeck.make_deck();
-    mydeck.shuffle_deck();
-    card drawn_card = mydeck.draw_card();
-    std::cout << drawn_card.rank << " of " << drawn_card.suit;
+    war war_game("bot", "Undeciphered", true);
+    war_game.setup_game();
+    war_game.start_game();
     
     return 0;
 }
